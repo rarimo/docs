82942ce96aa45ed1482153dd9f98dfa5f2f7c314
Tim McMackin <timothymcmackin@yahoo.com>
2023-05-29 09:38:00 -0400

    try with Layout component wrap

--- src/pages/testredirect.tsx
+++ src/pages/testredirect.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import {Redirect} from '@docusaurus/router';
+import Layout from "@theme/Layout";
 
 export default function Home(): JSX.Element {
-  return <Redirect to="/overview" />;
+  return (
+    <Layout>
+      <Redirect to="/overview" />
+    </Layout>
+  );
 };
