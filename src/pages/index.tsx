babe12d4b354974d7403011c29a0915f16b65fa2
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2023-05-29 16:14:33 +0300

    reverted disablings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-// import "aos/dist/aos.css";
+import "aos/dist/aos.css";
 
-// import BrowserOnly from "@docusaurus/BrowserOnly";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 // import HomepageDeveloperLinks from "@site/src/components/HomepageDeveloperLinks";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
@@ -8,9 +8,9 @@ import HomepageHeader from "@site/src/components/HomepageHeader";
 import HomepageLinks from "@site/src/components/HomepageLinks";
 import ArrowSvg from "@site/static/img/arrow_forward.svg";
 import Layout from "@theme/Layout";
-// import aos from "aos";
+import aos from "aos";
 import clsx from "clsx";
-import React from "react"; // useRef, // useEffect,
+import React, { useEffect, useRef } from "react";
 
 import styles from "./index.module.css";
 
@@ -54,88 +54,88 @@ function HomepageStartBuilding() {
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
-  // const light = useRef(null);
+  const light = useRef(null);
 
-  // useEffect(() => {
-  //   // const onMousemove = ({ pageX, pageY }) => {
-  //   //   const x = pageX;
-  //   //   const y = pageY;
-  //   //   const spotlightSize = "transparent 80px, rgba(0, 0, 0, 0.6) 200px)";
-  //   //   if (light.current) {
-  //   //     light.current.style.background = `radial-gradient(circle at ${x}px ${y}px, ${spotlightSize}`;
-  //   //   }
-  //   // };
-  //   aos.init({
-  //     duration: 900,
-  //     easing: "ease",
-  //     offset: 0,
-  //     once: true,
-  //     mirror: false,
-  //     anchorPlacement: "center-bottom",
-  //   });
-  //   // window.addEventListener("mousemove", onMousemove);
+  useEffect(() => {
+    const onMousemove = ({ pageX, pageY }) => {
+      const x = pageX;
+      const y = pageY;
+      const spotlightSize = "transparent 80px, rgba(0, 0, 0, 0.6) 200px)";
+      if (light.current) {
+        light.current.style.background = `radial-gradient(circle at ${x}px ${y}px, ${spotlightSize}`;
+      }
+    };
+    aos.init({
+      duration: 900,
+      easing: "ease",
+      offset: 0,
+      once: true,
+      mirror: false,
+      anchorPlacement: "center-bottom",
+    });
+    window.addEventListener("mousemove", onMousemove);
 
-  //   // return () => {
-  //   //   window.removeEventListener("mousemove", onMousemove);
-  //   // };
-  // }, []);
+    return () => {
+      window.removeEventListener("mousemove", onMousemove);
+    };
+  }, []);
 
   return (
     <Layout
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      {/* <BrowserOnly>
-        {() => ( */}
-      <>
-        <HomepageHeader />
-        <div className={clsx("bgImageFirst", styles.bgImageFirst)} />
-        <div
-          className={clsx("bgImageSecond", styles.bgImageSecond)}
-          // ref={light}
-        />
-        <div
-          data-aos="fade-up"
-          className={clsx(
-            "bgImageThird squareWaveAnimationDown",
-            styles.bgImageThird
-          )}
-        />
-        <div
-          data-aos="fade-up"
-          className={clsx(
-            "bgImageFourth squareWaveAnimationUp",
-            styles.bgImageFourth
-          )}
-        />
-        <div
-          data-aos="fade-up"
-          className={clsx(
-            "bgImageFifth squareWaveAnimationUp",
-            styles.bgImageFifth
-          )}
-        />
-        <div
-          data-aos="fade-up"
-          className={clsx(
-            "bgImageSixth squareWaveAnimationDown",
-            styles.bgImageSixth
-          )}
-        />
-        <main>
-          <HomepageFeatures />
-          {/*Todo: Show developer links */}
-          {/*<HomepageDeveloperLinks />*/}
-          <div className="container">
-            <div className={clsx("bottomContent", styles.bottomContent)}>
-              <HomepageStartBuilding />
-              <HomepageLinks />
-            </div>
-          </div>
-        </main>
-      </>
-      {/* )}
-      </BrowserOnly> */}
+      <BrowserOnly>
+        {() => (
+          <>
+            <HomepageHeader />
+            <div className={clsx("bgImageFirst", styles.bgImageFirst)} />
+            <div
+              className={clsx("bgImageSecond", styles.bgImageSecond)}
+              // ref={light}
+            />
+            <div
+              data-aos="fade-up"
+              className={clsx(
+                "bgImageThird squareWaveAnimationDown",
+                styles.bgImageThird
+              )}
+            />
+            <div
+              data-aos="fade-up"
+              className={clsx(
+                "bgImageFourth squareWaveAnimationUp",
+                styles.bgImageFourth
+              )}
+            />
+            <div
+              data-aos="fade-up"
+              className={clsx(
+                "bgImageFifth squareWaveAnimationUp",
+                styles.bgImageFifth
+              )}
+            />
+            <div
+              data-aos="fade-up"
+              className={clsx(
+                "bgImageSixth squareWaveAnimationDown",
+                styles.bgImageSixth
+              )}
+            />
+            <main>
+              <HomepageFeatures />
+              {/*Todo: Show developer links */}
+              {/*<HomepageDeveloperLinks />*/}
+              <div className="container">
+                <div className={clsx("bottomContent", styles.bottomContent)}>
+                  <HomepageStartBuilding />
+                  <HomepageLinks />
+                </div>
+              </div>
+            </main>
+          </>
+        )}
+      </BrowserOnly>
     </Layout>
   );
 }
