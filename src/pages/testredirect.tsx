55143099bd4038226292576d55906fa35a8d15bd
Tim McMackin <timothymcmackin@yahoo.com>
2023-05-30 10:01:50 -0400

    Revert "try with Layout component wrap"
    This reverts commit 82942ce96aa45ed1482153dd9f98dfa5f2f7c314.

--- src/pages/testredirect.tsx
+++ src/pages/testredirect.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import {Redirect} from '@docusaurus/router';
-import Layout from "@theme/Layout";
 
 export default function Home(): JSX.Element {
-  return (
-    <Layout>
-      <Redirect to="/overview" />
-    </Layout>
-  );
+  return <Redirect to="/overview" />;
 };
