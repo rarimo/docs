c6bcf3e1951c6f2fa58d866ae009ab8b025b0168
Oleksandr Varchenko <88536216+alexvarko@users.noreply.github.com>
2024-03-14 18:00:13 +0200

    Added react-medium-image-zoom (#33)
    * Added react-medium-image-zoom
    
    * Updated CHANGELOG.md

--- src/components/IdealImage/index.tsx
+++ src/components/IdealImage/index.tsx
@@ -1,17 +1,21 @@
 import Image, { Props as ImageProps } from '@theme/IdealImage'
 import React from 'react'
-
 import styles from './styles.module.css'
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
+
 
 export default function IdealImage({ img, alt, ...rest }: ImageProps) {
   return (
-    <Image
-      className={styles.idealImage}
-      // @ts-expect-error: `shouldAutoDownload` is not in the type definition
-      shouldAutoDownload={() => true}
-      img={img}
-      alt={alt}
-      {...rest}
-    />
+    <Zoom>
+      <Image
+        className={styles.idealImage}
+        // @ts-expect-error: `shouldAutoDownload` is not in the type definition
+        shouldAutoDownload={() => true}
+        img={img}
+        alt={alt}
+        {...rest}
+      />
+    </Zoom>
   )
 }
