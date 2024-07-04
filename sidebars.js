b123aab80ada1756476e38ab9daeb9a8295c47bf
Ihor Diachenko <ih.diachenko@gmail.com>
2024-07-04 14:29:17 +0300

    Update documentation structure (#47)
    * rm landing
    
    * Tweaked structure
    
    * identity => credential trees
    
    * Base layer -> Rarimo Core
    
    * products/rarime -> rarime
    
    * Passport-derived profiles -> accounts
    
    * Added commitment trees
    
    * Aggregator
    
    * Freedom tool up
    
    * Freedom tool up
    
    * tweaked menu styles
    
    * Unwrap use cases
    
    * tweak menu appearance
    
    * Fixed Rarimo core issue
    
    * Added changelog

--- sidebars.js
+++ sidebars.js
@@ -6,81 +6,73 @@ const config = require('./docusaurus.config')
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   sidebar: [
-    'overview',
-    {
-      type: 'category',
-      label: 'Use Cases',
-      link: {
-        type: 'generated-index',
-        title: 'Use Cases',
-        description: 'Learn about the different use cases for Rarimo Protocol',
-        slug: '/use-cases',
-      },
-      collapsed: true,
-      items: [
-        'use-cases/airdrop',
-        'use-cases/age-restricted-content',
-        'use-cases/self-sovereign-identity',
-        'use-cases/identity-protocol-state-replication',
-        'use-cases/organizational-access-control',
-        'use-cases/gaming-profiles',
-        'use-cases/social-graphs',
-      ],
-    },
     {
       type: 'html',
       value: `
-        <div class="menu__splitter"></div>
+        <span class="menu__link menu__link--no-hover">ZK Social Protocol</span>
       `
     },
     {
-      type: 'html',
-      value: `
-        <span class="menu__link menu__link--no-hover">Products</span>
-      `
+      type: 'doc',
+      label: 'Overview',
+      id: 'overview',
+    },
+    {
+      // formely: products/passport-derived-profiles
+      type: 'category',
+      label: 'Accounts',
+      link: { type: 'doc', id: 'accounts/overview' },
+      collapsed: true,
+      items: ['accounts/smart-contracts-reference'],
     },
     {
       type: 'category',
-      label: 'Base Layer',
-      link: {type: 'doc', id: 'products/base-layer/overview'},
+      label: 'Commitment trees',
+      link: { type: 'doc', id: 'commitment-trees/overview' },
       collapsed: true,
       items: [
-        'products/base-layer/rarimo-core',
-        'products/base-layer/oracles',
-        'products/base-layer/threshold-signature',
-        'products/base-layer/validator-node-operator-guide',
-        'products/base-layer/staking-guide',
-        'products/base-layer/api-reference',
+        'commitment-trees/commitments',
+        'commitment-trees/statement-trees',
+        'commitment-trees/time-trees',
       ],
     },
     {
+      // formely products/identity-protocol
       type: 'category',
-      label: 'Passport-Derived Profiles',
-      link: { type: 'doc', id: 'products/passport-derived-profiles/overview' },
-      collapsed: true,
-      items: ['products/passport-derived-profiles/smart-contracts-reference'],
-    },
-    {
-      type: 'category',
-      label: 'Identity Protocol',
-      link: {type: 'doc', id: 'products/identity-protocol/overview'},
+      label: 'Credential trees',
+      link: {type: 'doc', id: 'credential-trees/overview'},
       collapsed: true,
       items: [
-        'products/identity-protocol/guide-how-to-set-up-credentials-issuer',
-        'products/identity-protocol/guide-how-to-verify-zero-knowledge-proof-in-a-smart-contract',
-        'products/identity-protocol/smart-contracts-reference',
-        'products/identity-protocol/js-sdk-reference',
+        'credential-trees/guide-how-to-set-up-credentials-issuer',
+        'credential-trees/guide-how-to-verify-zero-knowledge-proof-in-a-smart-contract',
+        'credential-trees/smart-contracts-reference',
+        'credential-trees/js-sdk-reference',
       ],
     },
     {
+      // formely: products/base-layer
       type: 'category',
-      label: 'RariMe',
-      link: {type: 'doc', id: 'products/rarime/overview'},
+      label: 'Rarimo Core',
+      link: { type: 'doc', id: 'rarimo-core/overview' },
       collapsed: true,
       items: [
-        'products/rarime/guide-for-issuers',
-        'products/rarime/interoperability-with-external-credentials',
-        'products/rarime/js-connector-reference',
+        'rarimo-core/oracles',
+        'rarimo-core/threshold-signature',
+        'rarimo-core/validator-node-operator-guide',
+        'rarimo-core/staking-guide',
+        'rarimo-core/api-reference',
+      ],
+    },
+    {
+      // formely: products/rarime
+      type: 'category',
+      label: 'RariMe App',
+      link: {type: 'doc', id: 'rarime/overview'},
+      collapsed: true,
+      items: [
+        'rarime/guide-for-issuers',
+        'rarime/interoperability-with-external-credentials',
+        'rarime/js-connector-reference',
       ],
     },
 
@@ -93,8 +85,37 @@ const sidebars = {
     {
       type: 'html',
       value: `
+        <span class="menu__link menu__link--no-hover">Use cases</span>
+      `
+    },
+    'use-cases/airdrop',
+    'use-cases/age-restricted-content',
+    'use-cases/identity-protocol-state-replication',
+    'use-cases/organizational-access-control',
+    // 'use-cases/self-sovereign-identity',
+    //'use-cases/gaming-profiles',
+    //'use-cases/social-graphs',
+
+    {
+      type: 'html',
+      value: `
+        <div class="menu__splitter"></div>
+      `
+    },
+    {
+      type: 'html',
+      value: `
         <span class="menu__link menu__link--no-hover">Ecosystem</span>
       `
+    },
+    {
+      type: 'category',
+      label: 'Freedom Tool',
+      link: { type: 'doc', id: 'ecosystem/freedom-tool/overview' },
+      collapsed: true,
+      items: [
+        'ecosystem/freedom-tool/smart-contracts-reference',
+      ],
     },
     {
       type: 'category',
@@ -127,16 +148,6 @@ const sidebars = {
     },
     {
       type: 'category',
-      label: 'Freedom Tool',
-      link: {type: 'doc', id: 'ecosystem/freedom-tool/overview'},
-      collapsed: true,
-      items: [
-        // 'ecosystem/freedom-tool/guide-how-to-use-freedom-tool',
-         'ecosystem/freedom-tool/smart-contracts-reference',
-      ],
-    },
-    {
-      type: 'category',
       label: 'Polygon ID State Replication',
       link: {type: 'doc', id: 'ecosystem/polygon-id-state-replication/overview'},
       collapsed: true,
@@ -159,6 +170,7 @@ const sidebars = {
         'ecosystem/worldcoin-state-replication/api-reference',
       ],
     },
+
     {
       type: 'html',
       value: `
@@ -166,23 +178,18 @@ const sidebars = {
       `
     },
     {
-      type: 'category',
-      label: 'Resources',
-      link: {
-        type: 'generated-index',
-        slug: '/resources',
-      },
-      collapsed: true,
-      items: [
-        {
-          type: 'link',
-          label: 'Whitepaper',
-          href: config.customFields.whitepaperUrl,
-        },
-        'resources/audits',
-        'resources/glossary',
-      ],
+      type: 'html',
+      value: `
+        <span class="menu__link menu__link--no-hover">Resources</span>
+      `
     },
+    {
+      type: 'link',
+      label: 'Whitepaper',
+      href: config.customFields.whitepaperUrl,
+    },
+    'resources/audits',
+    'resources/glossary',
   ]
 }
 
