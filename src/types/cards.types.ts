113be8f47a1c81ee7d35c304f58d53547ed78499
ardier16 <ardier16@gmail.com>
2025-03-24 16:53:01 +0200

    Remove unused files

--- src/types/cards.types.ts
+++
@@ -1,9 +0,0 @@
-export type HomeCard = {
-  id?: string
-  title: string
-  IconName: React.ComponentType<React.ComponentProps<'svg'>>
-  list: React.ReactNode[][]
-  url: string
-  after?: JSX.Element
-  className?: string
-}
